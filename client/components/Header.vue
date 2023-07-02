15c9d1abc8d0771b4fdda6292529d92d9a353edc
Meowinn <darwinfagarang@gmail.com>
2023-07-02 16:18:16 +0800

    add additional styling and fix vmodel input

---
+++ client/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="bg-deep-orange-darken-2 py-3">
+        <v-card-title>{{ days[day] }}, {{ date }}</v-card-title>
+        <v-card-subtitle>{{ months[currentDate.getMonth()] }}</v-card-subtitle>
+    </div>
+</template>
+
+<script setup lang="ts">
+const currentDate = new Date();
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+let day = currentDate.getDay();
+const date = `${currentDate.getDate()}`
+
+
+</script>
--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,25 +1,31 @@
 <template>
 	<v-app>
 		<v-card width="500" class="mx-auto my-3">
-			<div class="bg-deep-orange-darken-2 py-3">
-				<v-card-title>{{ days[day] }}, {{ date }}</v-card-title>
-				<v-card-subtitle>{{ months[currentDate.getMonth()] }}</v-card-subtitle>
-			</div>
-
-			<v-alert v-if="alertToggle" type="success">
-				<v-alert-title>All tasks completed, Nice!</v-alert-title>
-			</v-alert>
+			<Header />
 
 			<v-container fluid class="bg-grey-lighten-3">
 				<v-form @submit.prevent>
 					<v-text-field v-model="modelTask.newTask" :rules="modelTask.rules" density="compact" clearable
 						label="Your tasks" type="text" variant="outlined" color="red align-self-center">
 						<template #append>
-							<v-btn icon="mdi-plus" @click="addNewTask" type="submit" color="orange-darken-4"></v-btn>
+							<v-btn icon="mdi-plus" @click="addNewTask" type="submit" color="orange-darken-1"></v-btn>
 						</template>
 					</v-text-field>
 				</v-form>
 			</v-container>
+
+			<v-alert v-if="alertToggle" type="success">
+				<v-alert-title>All tasks completed, Nice!</v-alert-title>
+			</v-alert>
+
+			
+			<v-row class="my-2 mx-3">
+				<strong class="ml-3">Remaining: {{ remainingTasks }}</strong>
+				<strong class="ml-6">Completed: {{ completedTasks }}</strong>
+				<v-spacer />
+				<v-progress-circular v-model="progress" :color="progress==100 ? 'green' : 'orange-darken-2	'"></v-progress-circular>
+			</v-row>
+
 			<v-divider></v-divider>
 			<v-card-text>
 				<v-list-item class="ma-0 pa-0" link :ripple="false" v-for="(allTask, index) in allTasks" :key="index">
@@ -28,25 +34,27 @@
 							<p :class="allTask.isDone ? 'strike' : ''" class="align-self-center">{{ allTask.task }}</p>
 						</template>
 						<template #append>
+
 							<v-dialog width="344" persistent>
 								<template #activator="{ props }">
 									<v-btn v-bind="props" variant="text" color="yellow-darken-3" icon="mdi-pencil"
-										size="small"></v-btn>
+										size="small" @click="edited=allTask.task"></v-btn>
 								</template>
 								<template #default="{ isActive }">
 									<v-card>
 										<v-toolbar color="warning" title="Edit"></v-toolbar>
 										<v-card-text class="pb-0">
-											<v-text-field v-model="allTask.task" clearable variant="outlined"
-												placeholder="Previous value here" label="Your task here"></v-text-field>
+											<v-text-field v-model="edited" clearable variant="outlined"
+												label="Your task here"></v-text-field>
 										</v-card-text>
 										<v-card-actions class="justify-end">
 											<v-btn @click="isActive.value = !isActive">Close</v-btn>
-											<v-btn @click="isActive.value = !isActive">Save</v-btn>
+											<v-btn @click="allTask.task=edited; isActive.value = !isActive">Save</v-btn>
 										</v-card-actions>
 									</v-card>
 								</template>
 							</v-dialog>
+
 
 							<v-btn variant="text" icon="mdi-close" color="error" size="small"
 								@click="deleteTask(allTask.id)"></v-btn>
@@ -59,18 +67,13 @@
 </template>
 <script lang="ts" setup>
 
-const currentDate = new Date();
-const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-let day = currentDate.getDay();
-const date = `${currentDate.getDate()}th`
 
 const id = ref(4)
 
 const modelTask = reactive({
 	newTask: '',
 	rules: [
-		value => {
+		(value: boolean) => {
 			if (value) return true
 			return 'Enter a task'
 		},
@@ -114,6 +117,8 @@ function deleteTask(allTaskId: any) {
 	allTasks.splice(allTasks.findIndex(task => task.id === allTaskId), 1)
 }
 
+const edited = ref('');
+
 const alertToggle = ref(false)
 
 watch(
@@ -126,6 +131,10 @@ watch(
 		}
 	}
 )
+
+const completedTasks = computed(() => allTasks.filter(task => task.isDone).length)
+const remainingTasks = computed(()=> allTasks.length - completedTasks.value)
+const progress = computed(()=> (completedTasks.value / allTasks.length) * 100) 
 </script>
 
 <style scoped>
